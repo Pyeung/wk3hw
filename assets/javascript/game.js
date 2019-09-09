aa5428f1b60993e9c8dcabb0ecb36fb326bbd532
Patrick <patrick.yeung7@gmail.com>
2019-09-09 12:21:28 -0400

    javascript broke, don't know where

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,14 @@
-<script>
+document.getElementById("guessLeft").innerHTML = 9;
 
-var compChoice = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var compChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 document.onkeyup = function(event) {
     
     var userGuess = event.key;
 
-    var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    var compGuess = compChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    if ((userGuess == "a") || (userGuess == "b") || (userGuess == "c") || (userGuess == "d") || (userGuess == "e") || (userGuess == "f") ||  (userGuess == "g") || (userGuess == "h") || (userGuess == "i") || (userGuess == "j") || (userGuess == "k") || (userGuess == "l") ||    (userGuess == "m") || (userGuess == "n") || (userGuess == "o") || (userGuess == "p") || (userGuess == "q") || (userGuess == "r") ||    (userGuess == "s") || (userGuess == "t") || (userGuess == "u") ||(userGuess == "v") || (userGuess == "w") || (userGuess == "x") ||     (userGuess == "y") || (userGuess == "z")) {
-​
-    alert("User guess: " + userGuess);
-    alert("Computer guess: " + computerGuess);
-​
-  }
-};
-
-
-</script>
+    function userWins () {
+      for (i = 0, i < computerChoices, i++);
+      console.log("Wins");
+    }
