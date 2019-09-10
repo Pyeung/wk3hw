bf29da4341b33a5862d0647759e3cb107364311d
Patrick <patrick.yeung7@gmail.com>
2019-09-09 22:07:18 -0400

    No progress made, listed all issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
+
 var winScore = 0
 var LooseScore = 0
 var tieScore = 0
 
+var titleText = document.getElementById("titleText");
 var playerChoiceText = document.getElementById("playerChoiceText");
 var compChoicesText = document.getElementById("compChoicesText");
 var winsText = document.getElementById("winsText");
@@ -15,24 +17,35 @@ document.onkeyup = function(event) {
 
   var playerGuess = event.key;
 
-  var compChoices = compChoices[Math.floor(Math.random() * compChoices.length)];
+  // Attempted the match function below to get a randomize letter from the array, somewhere in there is broke the script
 
-  if (playerGuess === compChoices) {
+  // var compChoices = compChoices[Math.floor(Math.random() * compChoices.length)];
 
+
+  // Below I tried to match player guess to computer choice in array, it does not work
+
+  if (playerGuess === compChoices.length) {
+
+
+    // 
      (i = 0, i < compChoices.length, i++);
-    console.log("Player win"); {
+    console.log('Player guess is ', compChoices[i]); {
       wins++;
       // else {
       //   lossses++;
-      // }
+      }
 
+
+      // Attempted to add text content to get by ID, something is not connecting. Nothing show on the HTML side
+
+      titleText.textContent = "<h1>The Psychic Game!</h1>";
       playerChoiceText.textContent = "You chose: " + playerGuess;
       compChoicesText.texContent = "The computer chose: " + compGuess;
       winsText.textContent = "Wins: " + wins;
       lossesText.textContent = "Losses: " + losses;
     }
   }
-}
+// }
 
 
   
